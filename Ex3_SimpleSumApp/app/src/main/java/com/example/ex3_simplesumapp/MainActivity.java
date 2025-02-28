9f339fa8de3047a8391a4f02e44b7aaba3cd4546
63133523-JavaProggramming <btruonganh2003@gmail.com>
2025-02-28 16:30:41 +0700

    Xử lý backend (OnClick)

--- Ex3_SimpleSumApp/app/src/main/java/com/example/ex3_simplesumapp/MainActivity.java
+++ Ex3_SimpleSumApp/app/src/main/java/com/example/ex3_simplesumapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.ex3_simplesumapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,24 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    //Đây là bộ lắng nghe và xử lý sự kiện Click trên nút Tính Tổng
+    public void XuLyCong(View view) {
+        //Tìm, tham chieus đến điều khiển trên tệp XMl, mapping qua java file
+        EditText editTextSoA = findViewById(R.id.editA);
+        EditText editTextSoB = findViewById(R.id.editB);
+        EditText editTextKetQua = findViewById(R.id.editKQ);
+
+        //Lay giá trị cua EditText
+        String soA = editTextSoA.getText().toString();
+        String soB = editTextSoB.getText().toString();
+
+        //Tinh tong
+        int tong = Integer.parseInt(soA) + Integer.parseInt(soB);
+        String ketQua = Integer.toString(tong);
+        //Hien thi ket qua
+        editTextKetQua.setText(tong + "");
+
     }
 }
