71fd3116e883606b21faf975fe358485f11e765d
63133523-JavaProggramming <btruonganh2003@gmail.com>
2025-03-10 19:35:59 +0700

    Viết code

--- VD1_ExplicitIntents/app/src/main/java/com/example/vd1_explicitintents/SubActivityOne.java
+++ VD1_ExplicitIntents/app/src/main/java/com/example/vd1_explicitintents/SubActivityOne.java
@@ -1,6 +1,8 @@
 package com.example.vd1_explicitintents;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,14 @@ public class SubActivityOne extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    // Hàm đáp ứng sự kiện nhấn lên nút "TRANG CHÚ"
+    // Xử lý chuyển màn hình
+    public void QuayVe(View v) {
+        // Tạo một đối tượng Intent
+        // Tham số thứ 2 của hàm tạo này, là tên Activity (màn hình) ta muốn chuyển sang
+        Intent iManHinhChinh = new Intent(this, MainActivity.class);
+        // Thực hành chuyển
+        startActivity(iManHinhChinh);
     }
 }
