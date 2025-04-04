9a48cd9db3a6ec090401b8d577a7d0c12e497d7f
63133523-JavaProggramming <btruonganh2003@gmail.com>
2025-04-04 14:03:04 +0700

    Xay dung main

--- thigk2TruongAnhBinh/app/src/main/java/com/example/thigk2truonganhbinh/MainActivity.java
+++ thigk2TruongAnhBinh/app/src/main/java/com/example/thigk2truonganhbinh/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.thigk2truonganhbinh;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +17,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button btn2 = findViewById(R.id.btn2);
+        Button btn3 = findViewById(R.id.btn3);
+        Button btn4 = findViewById(R.id.btn4);
+        Button btnMe = findViewById(R.id.btnMe);
+
+        btn2.setOnClickListener(v -> {
+            openActivity(ActivityCN2.class);
         });
+        btn3.setOnClickListener(v -> {
+            openActivity(ActivityCN3.class);
+        });
+        btn4.setOnClickListener(v -> {
+            openActivity(ActivityCN4.class);
+        });
+        btnMe.setOnClickListener(v -> {
+            openActivity(ActivityAboutMe.class);
+        });
+    }
+
+    private void openActivity(Class<?> activityClass) {
+        Intent intent = new Intent(this, activityClass);
+        startActivity(intent);
     }
 }
